e353569beae10d50f723c23222f1a3f3807b10c2
Vignan Kurme <vignan37@gmail.com>
2025-08-08 00:49:44 +0530

    Solved search in a BST q

---
+++ BinarySearchTree/700SearchInABST.java
@@ -0,0 +1,69 @@
+
+// Return the node if you find it, and also optimize the search by 
+//checking if val is less or greater and move to respective subtree for furthur search
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val == val) {
+            return root;
+        }
+        // Since it's a BST, we can optimize the search
+        if (val < root.val) {
+            return searchBST(root.left, val);
+        } else {
+            return searchBST(root.right, val);
+        }
+    }
+}
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+ /*
+  # [**700. Search in a Binary Search Tree**](https://leetcode.com/problems/search-in-a-binary-search-tree/)
+
+You are given the `root` of a binary search tree (BST) and an integer `val`.
+
+Find the node in the BST that the node's value equals `val` and return the subtree rooted with that node. If such a node does not exist, return `null`.
+
+**Example 1:**
+
+![](https://assets.leetcode.com/uploads/2021/01/12/tree1.jpg)
+
+```
+Input: root = [4,2,7,1,3], val = 2
+Output: [2,1,3]
+
+```
+
+**Example 2:**
+
+![](https://assets.leetcode.com/uploads/2021/01/12/tree2.jpg)
+
+```
+Input: root = [4,2,7,1,3], val = 5
+Output: []
+
+```
+
+**Constraints:**
+
+- The number of nodes in the tree is in the range `[1, 5000]`.
+- `1 <= Node.val <= 107`
+- `root` is a binary search tree.
+- `1 <= val <= 107`
+  */
