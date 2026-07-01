82929fbc25f2f64910d77745cd62fb3c4ac46a98
Vkurme <vkurme@investcloud.com>
2026-07-01 18:29:41 +0530

    solved using line sweeep alog

--- Arrays/LineSweepAlgo/MaxPopulationInYear.java
+++ Arrays/LineSweepAlgo/MaxPopulationInYear.java
@@ -1,3 +1,85 @@
+// line sweep algorithm
+/*
+class Solution {
+    public int maximumPopulation(int[][] logs) {
+
+        // Each log creates 2 events:
+        // Birth  -> +1
+        // Death  -> -1
+        int[][] events = new int[logs.length * 2][2];
+
+        int idx = 0;
+        for (int[] log : logs) {
+            // Birth event
+            events[idx][0] = log[0];
+            events[idx][1] = 1;
+            idx++;
+
+            // Death event
+            events[idx][0] = log[1];
+            events[idx][1] = -1;
+            idx++;
+        }
+
+        // Sort by year.
+        // If same year, process death (-1) before birth (+1)
+        Arrays.sort(events, (a, b) -> {
+            if (a[0] != b[0]) {
+                return a[0] - b[0];
+            }
+            return a[1] - b[1]; // -1 comes before +1
+        });
+
+        int currentPopulation = 0;
+        int maxPopulation = 0;
+        int answer = 1950;
+
+        for (int[] event : events) {
+
+            currentPopulation += event[1];
+
+            if (currentPopulation > maxPopulation) {
+                maxPopulation = currentPopulation;
+                answer = event[0];
+            }
+        }
+
+        return answer;
+    }
+}
+*/
+
+// difference Array Technique
+class Solution {
+    public int maximumPopulation(int[][] logs) {
+        
+        int[] year = new int[2051];
+        
+		// O(n) -> n is log.length
+		
+        for(int[] log : logs){
+            
+            year[log[0]] += 1;
+            year[log[1]] -= 1;
+        }
+        
+        int maxNum = year[1950], maxYear = 1950;
+        
+		// O(100) -> 2050 - 1950 = 100
+
+        for(int i = 1951; i < year.length; i++){
+            year[i] += year[i - 1];  // Generating Prefix Sum
+            
+            if(year[i] > maxNum){
+                maxNum = year[i];
+                maxYear = i;
+            }
+        }
+        
+        return maxYear;
+    }
+}
+
 
 // difference Array Technique
 class Solution {
